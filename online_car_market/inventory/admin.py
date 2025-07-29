0208ad187cc215e754cd3bc0bc680021f827e141
Erdey <erdeysyoum@gmail.com>
2025-07-29 16:35:51 +0300

    car image view added to admin site

--- online_car_market/inventory/admin.py
+++ online_car_market/inventory/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Car, CarImage
+from django.utils.html import format_html
 
-# Register your models here.
+
+class CarImageInline(admin.TabularInline):  # or StackedInline if you prefer vertical
+    model = CarImage
+    extra = 1
+    fields = ['image_preview', 'image', 'is_featured', 'caption']
+    readonly_fields = ['image_preview']
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="height: 80px;"/>', obj.image.url)
+        return "No image"
+    image_preview.short_description = "Preview"
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['make', 'model', 'year', 'price', 'featured_image_preview']
+    inlines = [CarImageInline]
+
+    def featured_image_preview(self, obj):
+        featured = obj.images.filter(is_featured=True).first()
+        if featured and featured.image:
+            return format_html('<img src="{}" style="height: 80px;" />', featured.image.url)
+        return "No featured image"
+    featured_image_preview.short_description = "Featured Image"
+
+admin.site.register(Car, CarAdmin)
