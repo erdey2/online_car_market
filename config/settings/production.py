3b3d66a5efcf3f014a97b8a6953d9e0c9e2ae6b3
Erdey <erdeysyoum@gmail.com>
2025-07-31 17:53:32 +0300

    Add Render domain to ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,11 +10,18 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["online-car-market"])
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["online-car-market"])
+ALLOWED_HOSTS = [
+    'online-car-market.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
+
+
 
 # CACHES
 # ------------------------------------------------------------------------------
