2662535dfae23931d7e1f30db15943886e79fd42
Erdey <erdeysyoum@gmail.com>
2025-07-31 17:44:57 +0300

    Add Render domain to ALLOWED_HOSTS

--- config/settings/local.py
+++ config/settings/local.py
@@ -15,9 +15,9 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
-    'online-car-market.onrender.com'
-    'localhost'
-    '127.0.0.1'
+    'online-car-market.onrender.com',
+    'localhost',
+    '127.0.0.1',
 ]  # noqa: S104
 
 # CACHES
