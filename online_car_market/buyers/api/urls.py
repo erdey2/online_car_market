ec794c9b1a842bc2ecfd1448c761e947b5415a66
Erdey <erdeysyoum@gmail.com>
2025-08-02 00:02:12 +0300

    dealers added to urls

--- online_car_market/buyers/api/urls.py
+++ online_car_market/buyers/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BuyerViewSet  # example view
+from .views import BuyerViewSet, DealerProfileViewSet, RatingViewSet, LoyaltyProgramViewSet  # example view
 
 router = DefaultRouter()
 router.register(r'buyers', BuyerViewSet, basename='buyer')
+router.register(r'dealers', DealerProfileViewSet, basename='dealer')
+router.register(r'ratings', RatingViewSet, basename='rating')
+router.register(r'loyalty', LoyaltyProgramViewSet, basename='loyalty')
 
 urlpatterns = [
     path('', include(router.urls)),
