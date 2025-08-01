795a1e758feed330c45a638ebc77aa1f6731fc6f
Erdey <erdeysyoum@gmail.com>
2025-08-01 23:58:45 +0300

    broker listing added to urls

--- online_car_market/brokers/api/urls.py
+++ online_car_market/brokers/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BrokerViewSet  # example view
+from .views import BrokerViewSet, BrokerListingViewSet  # example view
 
 router = DefaultRouter()
 router.register(r'brokers', BrokerViewSet, basename='broker')
+router.register(r'brokers-listing', BrokerListingViewSet, basename='broker-listing')
 
 urlpatterns = [
     path('', include(router.urls)),
