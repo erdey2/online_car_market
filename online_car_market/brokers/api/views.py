598eaac9a9d0288610285c78ffb5007800dc3236
Erdey <erdeysyoum@gmail.com>
2025-08-08 17:54:29 +0300

    broker permission changed from function to class based

--- online_car_market/brokers/api/views.py
+++ online_car_market/brokers/api/views.py
@@ -1,18 +1,32 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from rolepermissions.permissions import register_object_checker
+from rest_framework.permissions import BasePermission
 from rolepermissions.checkers import has_role
 from ..models import Broker, BrokerListing
 from .serializers import BrokerSerializer, BrokerListingSerializer
 from drf_spectacular.utils import extend_schema, extend_schema_view
 
-@register_object_checker()
-def has_manage_brokers_permission(permission, user, obj):
-    return has_role(user, ['super_admin', 'admin']) or (obj and obj.user == user)
 
-@register_object_checker()
-def has_manage_broker_listings_permission(permission, user, obj):
-    return has_role(user, ['super_admin', 'admin']) or (obj and obj.broker.user == user)
+class CanManageBrokers(BasePermission):
+    def has_permission(self, request, view):
+        # Optionally allow everyone to create brokers, or restrict here
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            has_role(request.user, ['super_admin', 'admin']) or
+            obj.user == request.user
+        )
+
+class CanManageBrokerListings(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            has_role(request.user, ['super_admin', 'admin']) or
+            obj.broker.user == request.user
+        )
 
 @extend_schema_view(
     list=extend_schema(tags=["brokers"]),
@@ -29,7 +43,7 @@ class BrokerViewSet(ModelViewSet):
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
-            return [IsAuthenticated(), has_manage_brokers_permission]
+            return [IsAuthenticated(), CanManageBrokers()]
         return [IsAuthenticated()]
 
     def get_queryset(self):
@@ -55,7 +69,7 @@ class BrokerListingViewSet(ModelViewSet):
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
-            return [IsAuthenticated(), has_manage_broker_listings_permission]
+            return [IsAuthenticated(), CanManageBrokerListings()]
         return [IsAuthenticated()]
 
     def get_queryset(self):
