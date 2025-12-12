846e4c69fd17462f7bbc713aba8d57ef42d0298d
Erdey <erdeysyoum@gmail.com>
2025-12-12 13:49:06 +0300

    allow seller to see leads

--- online_car_market/sales/api/urls.py
+++ online_car_market/sales/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import SaleViewSet, LeadViewSet
 
 router = DefaultRouter()
-router.register(r'', SaleViewSet, basename='sale')
 router.register(r'leads', LeadViewSet, basename='lead')
+router.register(r'', SaleViewSet, basename='sale')
 
 urlpatterns = [
     path('', include(router.urls)),
