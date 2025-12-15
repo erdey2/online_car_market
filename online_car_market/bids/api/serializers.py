7d9d4690b999a84fc87079f1dfada2159f04a6b7
Erdey <erdeysyoum@gmail.com>
2025-12-15 15:49:56 +0300

    car and profile added to bids

--- online_car_market/bids/api/serializers.py
+++ online_car_market/bids/api/serializers.py
@@ -1,38 +1,34 @@
 from rest_framework import serializers
 from ..models import Bid
 from online_car_market.inventory.models import Car
+from online_car_market.users.models import Profile
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class BidSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name', 'contact']
 
-    first_name = serializers.CharField(
-        source="user.profile.first_name",
-        read_only=True
-    )
-    last_name = serializers.CharField(
-        source="user.profile.last_name",
-        read_only=True
-    )
-    contact = serializers.CharField(
-        source="user.profile.contact",
-        read_only=True
-    )
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ['make', 'model']
+
+class BidSerializer(serializers.ModelSerializer):
+    # user = serializers.PrimaryKeyRelatedField(read_only=True)
+    profile = ProfileSerializer(source='user.profile', read_only=True)
+    car = CarSerializer(source='car', read_only=True)
+    #first_name = serializers.CharField(source="user.profile.first_name", read_only=True)
+    #last_name = serializers.CharField(source="user.profile.last_name", read_only=True)
+    #contact = serializers.CharField(source="user.profile.contact", read_only=True)
+    #make = serializers.CharField(source="car.make", read_only=True)
+    #model = serializers.CharField(source="car.model", read_only=True)
 
     class Meta:
         model = Bid
-        fields = [
-            'id',
-            'car',
-            'user',
-            'amount',
-            'created_at',
-            'first_name',
-            'last_name',
-            'contact',
-        ]
+        fields = ['id','car', 'user','profile', 'amount','created_at']
         read_only_fields = ['id', 'user', 'created_at']
 
     def validate_car(self, value):
