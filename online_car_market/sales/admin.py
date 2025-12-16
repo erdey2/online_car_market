1b2af2a711058d98c861df52ed883bea76e0340b
Erdey <erdeysyoum@gmail.com>
2025-12-16 21:49:32 +0300

    sale and lead add to admin

--- online_car_market/sales/admin.py
+++ online_car_market/sales/admin.py
@@ -3,3 +3,14 @@ from .models import Sale, Lead
 
 admin.register(Sale)
 admin.register(Lead)
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ("id", "car", "buyer", "price", "date", "broker", "dealer")
+    search_fields = ("car", "price")
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "contact", "status", "assigned_sales", "car", "created_at")
+    search_fields = ("car", "contact")
+
