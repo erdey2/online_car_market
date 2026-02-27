29fdf77749bc11db94e27a96b44ecde2757cf862
Erdey <erdeysyoum@gmail.com>
2026-02-27 14:33:13 +0300

    car detail serializer updated

--- online_car_market/inventory/api/serializers.py
+++ online_car_market/inventory/api/serializers.py
@@ -241,32 +241,6 @@ class CarDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Car
-        ''' fields = [
-            "id",
-            "vin",
-            "origin",
-            "make",
-            "model",
-            "year",
-            "price",
-            "mileage",
-            "fuel_type",
-            "body_type",
-            "drivetrain",
-            "condition",
-            "status",
-            "sale_type",
-            "auction_end",
-            "description",
-            "images",
-            "bids",
-            "seller",
-            "bid_count",
-            "highest_bid",
-            "seller_average_rating",
-            "features",
-            "created_at",
-        ] '''
         fields = '__all__'
 
     def get_seller(self, obj):
@@ -300,12 +274,61 @@ class CarDetailSerializer(serializers.ModelSerializer):
         ]
 
     def get_features(self, obj):
-        return {
-            field.name: getattr(obj, field.name)
-            for field in obj._meta.fields
-            if isinstance(field, models.BooleanField)
-               and getattr(obj, field.name)
-        }
+        feature_fields = [
+            "bluetooth",
+            "heated_seats",
+            "cd_player",
+            "power_locks",
+            "premium_wheels_rims",
+            "winch", "alarm_anti_theft",
+            "cooled_seats",
+            "keyless_start",
+            "body_kit",
+            "navigation_system",
+            "premium_lights",
+            "cassette_player",
+            "fog_lights",
+            "leather_seats",
+            "roof_rack",
+            "dvd_player",
+            "power_mirrors",
+            "power_sunroof",
+            "aux_audio_in",
+            "brush_guard",
+            "air_conditioning",
+            "performance_tyres",
+            "premium_sound_system",
+            "heat", "vhs_player",
+            "off_road_kit",
+            "am_fm_radio",
+            "moonroof",
+            "racing_seats",
+            "premium_paint",
+            "spoiler",
+            "power_windows",
+            "sunroof",
+            "climate_control",
+            "parking_sensors",
+            "rear_view_camera",
+            "keyless_entry",
+            "off_road_tyres",
+            "satellite_radio",
+            "power_seats",
+            "tiptronic_gears",
+            "dual_exhaust",
+            "power_steering",
+            "cruise_control",
+            "all_wheel_steering",
+            "front_airbags",
+            "side_airbags",
+            "n2o_system",
+            "anti_lock_brakes",
+        ]
+        return [
+            field.replace("_", " ").title()
+            for field in feature_fields
+            if getattr(obj, field, False)
+        ]
 
 class CarWriteSerializer(serializers.ModelSerializer):
     dealer = serializers.PrimaryKeyRelatedField(
