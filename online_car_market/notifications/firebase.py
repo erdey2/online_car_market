bfd3d73323e4ca1a3f65285211fc4825867dec7b
Erdey <erdeysyoum@gmail.com>
2026-04-04 06:54:13 +0300

    firebase updated

--- online_car_market/notifications/firebase.py
+++ online_car_market/notifications/firebase.py
@@ -2,8 +2,19 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 
+
 def init_firebase():
+    cred_env = os.getenv("FIREBASE_CREDENTIALS_PATH")
+
+    if not cred_env:
+        raise ValueError("FIREBASE_CREDENTIALS_PATH is not set!")
+
+    cred_path = os.path.join(os.getcwd(), cred_env)
+
+    if not os.path.exists(cred_path):
+        raise FileNotFoundError(f"Firebase credentials not found at {cred_path}")
+
+    cred = credentials.Certificate(cred_path)
+
     if not firebase_admin._apps:
-        cred_path = os.path.join(os.getcwd(), os.getenv("FIREBASE_CREDENTIALS_PATH"))
-        cred = credentials.Certificate(cred_path)
         firebase_admin.initialize_app(cred)
