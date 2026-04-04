832013f89eca66cc59624171ab4f00d4cf85ef80
Erdey <erdeysyoum@gmail.com>
2026-04-04 15:54:10 +0300

    firebase settings updated

--- online_car_market/notifications/firebase.py
+++ online_car_market/notifications/firebase.py
@@ -1,19 +1,28 @@
-import os, json, base64
+import os
+import json
+import base64
 import firebase_admin
 from firebase_admin import credentials
 
 def init_firebase():
-    if not firebase_admin._apps:
-        b64 = os.getenv("FIREBASE_CREDENTIALS_B64")
+    if firebase_admin._apps:
+        print("Firebase already initialized")
+        return
 
-        if not b64:
-            print("Firebase not configured")
-            return
+    b64 = os.getenv("FIREBASE_CREDENTIALS_B64")
 
-        decoded = base64.b64decode(b64).decode("utf-8")
-        cred_dict = json.loads(decoded)
+    if not b64:
+        raise ValueError("FIREBASE_CREDENTIALS_B64 not found")
 
-        cred = credentials.Certificate(cred_dict)
+    try:
+        decoded = base64.b64decode(b64)
+        service_account_info = json.loads(decoded)
+
+        cred = credentials.Certificate(service_account_info)
         firebase_admin.initialize_app(cred)
 
-        print("Firebase initialized")
+        print("Firebase initialized successfully")
+
+    except Exception as e:
+        print("Firebase init failed")
+        print(str(e))
