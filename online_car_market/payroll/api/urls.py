47c7f96d3b2a213e0759a2a4498532dee6fc6ed3
Erdey <erdeysyoum@gmail.com>
2026-05-20 15:33:23 +0300

    payslip me added

--- online_car_market/payroll/api/urls.py
+++ online_car_market/payroll/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from online_car_market.payroll.api.views import PayrollRunViewSet, PayslipAPIView
+from online_car_market.payroll.api.views import PayrollRunViewSet, PayslipAPIView, PayslipMeAPIView
 
 router = DefaultRouter()
 router.register("payroll-runs", PayrollRunViewSet, basename="payroll-run")
@@ -9,5 +9,6 @@ router.register("payroll-runs", PayrollRunViewSet, basename="payroll-run")
 urlpatterns = [
     *router.urls,
     path("payslips/", PayslipAPIView.as_view(), name="payslip"),
+    path("payslips/me/", PayslipMeAPIView.as_view(), name="payslip-me"),
 ]
 
